e740bfb11f3cd0e1e0732890d6e8941df744433e
Jack Weyen <jack.weyen@pcgh.com>
2024-10-21 17:27:58 +0200

    Updated TODOs

--- lib/main.dart
+++ lib/main.dart
@@ -32,3 +32,22 @@ class PowerUpGolfApp extends StatelessWidget {
   }
 }
 
+// TODO
+//
+// Connect app to MySQL database
+//  Get AWS Lambda code deployed
+//  Connect to app
+//  Send test data
+//  
+// Get user and game tables set up - DONE
+//
+// Create "new game" functionality - DONE
+//  Creates new game, shows new game available
+//    Game ID, game code, game creator, player, hole 1, hole 1 powerup, ... hole 9 powerup
+//
+// Create "new user" functionality - DONE
+//  Creates new user
+//    User ID, username, email (encrypted), password (encrypted)
+//
+// Test out all lambda functions
+
